6fb9535f707d7e4c8aea642ed20c4d92edc01552
PreethamSagar <92980978+PreethamSagar@users.noreply.github.com>
2024-04-23 12:28:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,15 +15,12 @@ st.write('The name on Smoothie will be: ',title)
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 # st.dataframe(data = my_dataframe, use_container_width=True)
 ingredients_list = st.multiselect('Choose up to 5 ingredients:', my_dataframe, max_selections = 5)
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-# st.text(fruityvice_response)
-fv_df = st.data_editor(data = fruityvice_response.json(), use_container_width = True)
 if len(ingredients_list) != 0:
-    # st.write(ingredients_list)
-    # st.text(ingredients_list)
     ingredients_string = ''
     for fruit_choosen in ingredients_list:
         ingredients_string+=fruit_choosen+ ' '
+        fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{fruit_choosen}")
+        fv_df = st.data_editor(data = fruityvice_response.json(), use_container_width = True)
     # st.write(ingredients_string)
     my_insert_statement = "insert into smoothies.public.orders(ingredients, name_on_order) values ('"+ingredients_string+"','"+title+"')"
     submit_button = st.button('Submit Order')
